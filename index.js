4a539a63684d162f745e24fe60e9634b75c8d75d
jpoellot1 <j.poellot1@gmail.com>
2026-06-08 20:43:11 -0500

    save on 6-8

--- index.js
+++ index.js
@@ -1,95 +1,66 @@
 // http://www.omdbapi.com/?apikey=c3dc2346&
 // http://img.omdbapi.com/?apikey=c3dc2346&
 
-// let currentMovies = []
+async function getMovies(searchQuery) { 
+    try{
+        const response = await fetch (`http://www.omdbapi.com/?apikey=c3dc2346&s=${searchQuery}`)
+        const result = await response.json()
+        console.log(result)
+        return result.Search || []
+    } catch (error) {
+        console.error('Error fetching data:', error)
+        return []
+    }
+}
 
-    async function getMovies(searchQuery) {
-            try{
-                const response = await fetch (`http://www.omdbapi.com/?apikey=c3dc2346&s=${searchQuery}`)
-                const result = await response.json()
-                console.log(result)
-                return result.Search || []
-            } catch (error) {
-                console.error('Error fetching data:', error)
-                return []
-            }
+async function searchResults(searchQuery) {
+    const moviesWrapper = document.querySelector('.movies')
+    moviesWrapper.classList += ' loading__state'
+    
+    const movies = await getMovies(searchQuery)
+    console.log("movies received in UI", movies) 
+    
+    if (!Array.isArray(movies) || movies.length === 0) {
+        moviesWrapper.innerHTML = '<p>No Movies Found</p>'
+        return;
+    }
+     moviesWrapper.classList.remove('loading__state')
+     
+    const originalSixMovies = movies.slice(0,6)
+
+    const sortValue = document.getElementById('sort-select').value
+
+    if (sortValue === 'A_to_Z') {
+        originalSixMovies.sort((a,b) => a.Title.localeCompare(b.Title))
+    }
+    else if (sortValue === 'Z_to_A') {
+        originalSixMovies.sort((a,b) => b.Title.localeCompare(a.Title))
     }
 
-    async function searchResults(searchQuery) {
-        const moviesWrapper = document.querySelector('.movies')
-        moviesWrapper.classList += ' .loading__state--visible'
-        
-        const movies = await getMovies(searchQuery)
-        console.log("movies received in UI", movies)    
-        
-        if (!Array.isArray(movies) || movies.length === 0) {
-            moviesWrapper.innerHTML = '<p>No Movies Found</p>'
-            moviesWrapper.classList.remove('loading__state--visible');
-            return;
-        }
-
-        const movieHtml = movies
-            .slice(0,6)
-            .map((movie) => {
-                return `<div class="movie">
-                <img class="movie__poster" src="${movie.Poster}">
-                <h3 class="movie__title">${movie.Title}</h3>
-                <p class="movie__year">${movie.Year}</p>
-            </div>`
-        })
-        .join("")
-        moviesWrapper.innerHTML = movieHtml
-
-        moviesWrapper.classList.remove('loading__state--visible')
-    }
-
-    // async function renderMovies(moviesList) {
-    //     const moviesWrapper = document.querySelector('.movies')    
-
-    //     if (!Array.isArray(moviesList) || moviesList.length === 0) {
-    //         moviesWrapper.innerHTML = '<p>No Movies Found</p>'
-    //         return;
-    //     }
-
-    //     const movieHtml = moviesList
-    //         .slice(0,6)
-    //         .map((movie) => {
-    //             return `<div class="movie">
-    //             <img class="movie__poster" src="${movie.Poster}">
-    //             <h3 class="movie__title">${movie.Title}</h3>
-    //             <p class="movie__year">${movie.Year}</p>
-    //         </div>`
-    //     })
-    //     .join("")
-    //     moviesWrapper.innerHTML = movieHtml
-    // }
-
-    // function handleSort() {
-    //     const sortValue = document.getElementById('filter')
-
-    //     let sortedMovies = [...currentMovies]
-
-    //     if (sortValue === 'A_to_Z') {
-    //         sortedMovies.sort((a, b) =>
-    //         a.Title.localCompare(b.title))
-    //     }
-    //     else if (sortValue === 'Z_to_A') {
-    //         sortedMovies.sort((a, b) =>
-    //         b.Title.localCompare(a.title))
-    //     }
-    //     renderMovies(sortedMovies)
-    // }
-
-    function filterMovies(event) {
-        searchResults(event.target.value)
-    }
-
-
-    document.getElementById('search-form').addEventListener('submit', async function(event) {
-            event.preventDefault();
-            const searchQuery = document.getElementById('search-input').value
-
-            if (searchQuery.trim()) {
-                await searchResults(searchQuery)
-            }
+    const movieHtml = originalSixMovies
+        .map((movie) => {
+            return `<div class="movie">
+            <img class="movie__poster" src="${movie.Poster}">
+            <h3 class="movie__title">${movie.Title}</h3>
+            <p class="movie__year">${movie.Year}</p>
+        </div>`
     })
+    .join("")
+    moviesWrapper.innerHTML = movieHtml
+}
+
+function handleSort() {
+    const currentSearch = document.getElementById('search-input').value
+    if (currentSearch.trim()) {
+        searchResults(currentSearch)
+    }
+}
+
+document.getElementById('search-form').addEventListener('submit', async function(event) {
+        event.preventDefault();
+        const searchQuery = document.getElementById('search-input').value
+
+        if (searchQuery.trim()) {
+            await searchResults(searchQuery)
+        }
+})
